f09213a33413f183e4768760067b545b49210d36
baotran133 <ngaottb@gmail.com>
2022-08-13 18:14:35 +0700

    them try catch

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -4,45 +4,45 @@ const { successCode, errorCode, failCode } = require("../ulti/response");
 const model = initModel(sequelize);
 //them order
 const addOrder = async (req, res) => {
-    const { user_id, food_id, amount, code, arr_sub_id } = req.body;
-    const data_user = await model.user.findByPk(user_id);
-    const data_food = await model.food.findByPk(food_id);
-    const isExist = await model.order.findOne({
-        where: {
-            user_id,
-            food_id
+    try {
+        const { user_id, food_id, amount, code, arr_sub_id } = req.body;
+        const data_user = await model.user.findByPk(user_id);
+        const data_food = await model.food.findByPk(food_id);
+        const isExist = await model.order.findOne({
+            where: {
+                user_id,
+                food_id
+            }
+        });
+        //(table order nen co primary key order_id, đơn hàng có thể đặt trùng danh sách món cũ
+        if (isExist) {
+            errorCode(res, `Khong thanh cong, trung data`);
         }
-    });
-    //(table order nen co primary key order_id, đơn hàng có thể đặt trùng danh sách món cũ
-    if (isExist) {
-        errorCode(res, `Khong thanh cong, trung data`);
-    }
-    //user khong ton tai trong danh sach nha hang da luu
-    else if (!data_user) {
-        errorCode(res, "Tai khoan khong ton tai");
-    }
-    //food khong ton tai trong danh sach nha hang da luu
-    else if (!data_food) {
-        errorCode(res, "Mon an khong ton tai");
-    } else {
-        obj = {
-            user_id,
-            food_id,
-            amount,
-            code,
-            arr_sub_id
-        };
-        try {
+        //user khong ton tai trong danh sach nha hang da luu
+        else if (!data_user) {
+            errorCode(res, "Tai khoan khong ton tai");
+        }
+        //food khong ton tai trong danh sach nha hang da luu
+        else if (!data_food) {
+            errorCode(res, "Mon an khong ton tai");
+        } else {
+            obj = {
+                user_id,
+                food_id,
+                amount,
+                code,
+                arr_sub_id
+            };
+
             const data = await model.order.create(obj);
             if (data) {
                 successCode(res, "Da dat mon!");
             } else {
                 errorCode(res, "Dat mon khong thanh cong!");
             }
-        } catch {
-            //catch sai data type o req.body,...
-            failCode(res);
         }
+    } catch {
+        failCode(res);
     }
 };
 //Unlike
--- src/controllers/rateController.js
+++ src/controllers/rateController.js
@@ -4,37 +4,41 @@ const { successCode, errorCode, failCode } = require("../ulti/response");
 const model = initModel(sequelize);
 
 const createRate = async (req, res) => {
-    const { user_id, res_id, amount, date_rate } = req.body;
-    const data_user = await model.user.findByPk(user_id);
-    const data_res = await model.restaurant.findByPk(res_id);
-    const isExist = await model.rate_res.findOne({
-        where: {
-            user_id,
-            res_id
-        }
-    });
-    //data da ton tai ( user da rate nha hang nay, trung khoa chinh, thong bao da rate)
-    if (isExist) {
-        errorCode(res, "Da rate");
-        //nguoi dung chua dang nhap hoac khong ton tai
-    } else if (!data_user) {
-        errorCode(res, "Tai khoan khong ton tai");
-        //nha hang khong ton tai trong danh sach nha hang da luu
-    } else if (!data_res) {
-        errorCode(res, "Nha hang ko ton tai");
-    } else {
-        obj = {
-            user_id,
-            res_id,
-            amount,
-            date_rate
-        };
-        try {
+    try {
+        const { user_id, res_id, amount, date_rate } = req.body;
+        const data_user = await model.user.findByPk(user_id);
+        const data_res = await model.restaurant.findByPk(res_id);
+        const isExist = await model.rate_res.findOne({
+            where: {
+                user_id,
+                res_id
+            }
+        });
+        //data da ton tai ( user da rate nha hang nay, trung khoa chinh, thong bao da rate)
+        if (isExist) {
+            errorCode(res, "Da rate");
+            //nguoi dung chua dang nhap hoac khong ton tai
+        } else if (!data_user) {
+            errorCode(res, "Tai khoan khong ton tai");
+            //nha hang khong ton tai trong danh sach nha hang da luu
+        } else if (!data_res) {
+            errorCode(res, "Nha hang ko ton tai");
+        } else {
+            obj = {
+                user_id,
+                res_id,
+                amount,
+                date_rate
+            };
             const data = await model.rate_res.create(obj);
-            successCode(res, "Rate thanh cong!");
-        } catch {
-            failCode(res);
+            if (data) {
+                successCode(res, "Rate thanh cong!");
+            } else {
+                errorCode(res, "Rate khong thanh cong");
+            }
         }
+    } catch {
+        failCode(res);
     }
 };
 
